f3930dabfaa94216111cb6671d9f8a349dcfe377
Mateus Marques <mateusmarques2001@usp.br>
2022-09-30 13:11:02 -0300

    Detail in clean.sh and adding nca/README

--- nca/bin/clean.sh
+++ nca/bin/clean.sh
@@ -3,5 +3,5 @@
 # This script only cleans all output and backups (BE CAREFUL)
 HERE=$(dirname "$0")
 
-rm -rf "$HERE"/backup_sig
-rm -rf "$HERE"/output
+[ -d "$HERE"/backup_sig ] && rm -rf "$HERE"/backup_sig
+[ -d "$HERE"/output ] && rm -rf "$HERE"/output
