3831fdf476e5c0087cd78f34c3796532198c19c9
Mateus Marques <mateusmarques2001@usp.br>
2022-10-13 00:02:35 -0300

    Primeiros testes com o exemplo do nca-2004.

--- nca-2004/bin/README.md
+++ nca-2004/bin/README.md
@@ -8,3 +8,14 @@ To generate cix file:
   Give output filename?
      cix.dat
   Output written on cix.dat
+
+The file cix-oca.dat (by generate-oca) is equal to the example file cix4567-pc.cix.
+Because of this, the directories `output-4567` and `output-oca` are exactly equal.
+
+command to run:
+```
+./nca "out=./output" Sig=start/Sigma.000 Ac=start/Ac.12 cix=start/cix4567.cix U=4 T=0.1 "Ed={-20.6665403945682,-20.5769153075867,-20.6411607517974}"
+```
+
+Although output-2004 is not equal to output-4567 and output-oca, the plots of gloc.out are identical.
+The conclusion must be that all `generate` executables are compatible.
---
+++ nca-2004/bin/plot-example/plot-gloc.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+from matplotlib import pyplot as plt
+## comentar as 4 linhas abaixo caso nao tenha o LaTeX no matplotlib ###
+from matplotlib import rc
+plt.style.use('bmh')
+rc('font', **{'family': 'serif', 'serif': ['Computer Modern']})
+rc('text', usetex=True)
+#######################################################################
+
+import sys
+
+def spectral(ImG):
+    return -ImG / np.pi
+
+def main():
+    suffix = "-" + sys.argv[1]
+    filename = "gloc" + suffix + ".out"
+    # loading data
+    freq, ReG1, ImG1, ReG2, ImG2, ReG3, ImG3 = np.loadtxt(filename, unpack=True)
+    spectral_func1 = spectral(ImG1)
+    spectral_func2 = spectral(ImG2)
+    spectral_func3 = spectral(ImG3)
+    # plotting
+    plt.plot(freq,  spectral_func1, label=r'$A_1(\omega)$')
+    plt.plot(freq,  spectral_func2, label=r'$A_2(\omega)$')
+    plt.plot(freq,  spectral_func3, label=r'$A_3(\omega)$')
+    plt.xlabel(r'$\omega$', fontsize=20)
+    plt.ylabel(r'$A(\omega)$', fontsize=20)
+    plt.legend(fontsize=16)
+    plt.title(r'$A(\omega)$'+" of "+filename+" in the example")
+    plt.savefig("specfun-examp"+suffix+".png", dpi=300, format='png', bbox_inches="tight")
+    plt.clf()
+    return 0
+
+
+if __name__ == '__main__':
+    main()
